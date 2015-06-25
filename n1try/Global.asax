ae8686b2778929ba09d68ee522cc494bfe6588bc
ronakpromact <Ronak@promactinfo.com>
2015-06-25 10:51:36 +0530

    n1try msg

---
+++ n1try/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="n1try.MvcApplication" Language="C#" %>
---
+++ n1try/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(n1try.Startup))]
+namespace n1try
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
